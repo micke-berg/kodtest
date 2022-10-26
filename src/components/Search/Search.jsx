d0c5f480fe0932ba4bec5f14a10e21683b052ae0
Micke Berg <micke.berg@gmail.com>
2022-10-26 10:34:31 +0200

    simplify search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,28 +2,19 @@ import React, { useState } from 'react';
 import styles from './search.module.scss';
 import { MdClear, MdOutlineSearch } from 'react-icons/md';
 
-const Search = ({
-	filterGamesByCurrency,
-	dataToSearchByName,
-	data,
-	placeholder,
-	style,
-}) => {
+const Search = ({ filterGamesByCurrency, data, placeholder, style }) => {
 	const [searchTerm, setSearchTerm] = useState('');
 
 	const handleSearch = (searchText) => {
 		// Searh item by name
-		filterGamesByCurrency(data);
 		setSearchTerm(searchText);
 
-		const searchResult = dataToSearchByName.filter((item) =>
+		const searchResult = data.filter((item) =>
 			item.name.toLowerCase().includes(searchText.toLowerCase())
 		);
 
-		if (searchText.length > 1) {
+		if (searchText.length > 0) {
 			filterGamesByCurrency(searchResult);
-		} else if (searchText === '') {
-			filterGamesByCurrency(data);
 		}
 	};
 
