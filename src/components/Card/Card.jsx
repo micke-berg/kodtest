de281b30190a703be04e0d53f1047c8d3dc4e322
Micke Berg <micke.berg@gmail.com>
2022-10-26 21:40:14 +0200

    div to li

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,13 +3,13 @@ import styles from './card.module.scss';
 
 const Card = ({ title, subTitle, imageUrl, style, onClick }) => {
 	return (
-		<div className={styles.card} style={{ ...style }} onClick={onClick}>
+		<li className={styles.card} style={{ ...style }} onClick={onClick}>
 			<img className={styles.img} src={imageUrl} alt={title}></img>
 			<div className={styles.info}>
 				<div className={styles.title}>{title}</div>
 				<div className={styles.subTitle}>{subTitle}</div>
 			</div>
-		</div>
+		</li>
 	);
 };
 
